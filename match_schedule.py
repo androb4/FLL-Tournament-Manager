8e131c7e3fbf9a24264f1ed0148423eb968fa010
Andres Abrego <andres.r.abrego@gmail.com>
2015-11-28 06:00:46 -0600

    Replaced the Match class with a dictionary for conversion to JSON

--- match_schedule.py
+++ match_schedule.py
@@ -1,12 +1,9 @@
 matchList = []
 tableNames = []
 
-
-class Match:
-    matchNumber = None
-    tables = []
-    teams = []
-    def __init__(self):
-        self.matchNumber = None
-        self.tables = []
-        self.teams = []
+Match = {
+    'matchNumber': 0,
+    'matchTime': "",
+    'tables': [],
+    'teams': []
+}
--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ import thread
 import time
 import math
 import xlrd
+import copy
 
 import match_schedule
 
@@ -20,16 +21,11 @@ class MatchListUploadHandler(tornado.web.RequestHandler):
             match_schedule.tableNames.append(sheet.row_values(1)[t])
 
         for r in range(2, sheet.nrows):
-            match = match_schedule.Match()
+            match = copy.deepcopy(match_schedule.Match)
             for t in range(2, len(sheet.row_values(r))):
                 if sheet.row_values(r)[t] is not "":
-                    match.matchNumber = int(sheet.row_values(r)[0])
-                    match.teams.append(int(sheet.row_values(r)[t]))
-                    match.tables.append(match_schedule.tableNames[t-2])
+                    match['matchNumber'] = int(sheet.row_values(r)[0])
+                    match['matchTime'] = sheet.row_values(r)[1]
+                    match['teams'].append(int(sheet.row_values(r)[t]))
+                    match['tables'].append(match_schedule.tableNames[t-2])
             match_schedule.matchList.append(match)
-
-        for m in range(0, len(match_schedule.matchList)):
-            print "Match" + str(match_schedule.matchList[m].matchNumber),
-            for t in range(0, len(match_schedule.matchList[m].teams)):
-                print " " + match_schedule.matchList[m].tables[t] + " " + str(match_schedule.matchList[m].teams[t]),
-            print
